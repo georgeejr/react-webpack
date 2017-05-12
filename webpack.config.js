44148eecc5937e8cb1c7e5af7872de53e85f855b
AUSTIN\geoas_000 <geoash16@hotmail.com>
2017-05-12 19:19:56 +0800

    update webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path')
 
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: __dirname + '/dist',
+        path: path.resolve(__dirname,  'dist'),
         filename: 'app.bundle.js'
     },
     module: {
